2a97b15745db71007f9411a4eb4d63dfce135cae
vrtkarim <elyaagoubi.karim03@gmail.com>
2025-03-09 14:49:27 +0000

    idk

---
+++ src/test/java/com/ramadan/dayone/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramadan.dayone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
